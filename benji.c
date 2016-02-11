24489390dce1a6726df9a12c546d91fcd5b96758
Benjamin Budet <budet_b@epita.fr>
2016-02-11 14:15:24 -0500

    files ok

--- benji.c
+++ benji.c
@@ -21,6 +21,14 @@ int main(int argc, char* argv[]){
     ssize_t linelen;
     while ((linelen = getline(&line, &linecap, input)) > 0){
         maxchar[i] = linelen;
+        if (i == 0) {
+            row = maxchar[0];
+            columns = maxchar[1];
+            drones = maxchar[2];
+            deadline = maxchar[3];
+            maxload = maxchar[4];
+        }
         i++;
     }
+
 }
