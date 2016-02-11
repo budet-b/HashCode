d0df2ab904c7a15c031f6d2f839e5b70cedc8a6e
Zarbuz <nicolas.perrier2@numericable.fr>
2016-02-11 20:32:26 +0100

    Updated file nico.c

---
+++ nico.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct drone {
+    int id;
+    int x,y;
+    int maxWeight;
+    int actualWeight;
+
+}
+
+void cmd_Load(int id, char c, int idWarehouse, int idProductType, int numberItems)
+{
+    
+}
+
+void cmd_Deliver(int id, char c, int idCustomer, int idProductType, int numberItems)
+{
+
+}
