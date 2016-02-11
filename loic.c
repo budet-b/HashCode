883b4a1247573f9c2336a2366b9004b4b5874fbe
Loic Reyreaud <loic.reyreaud@epita.fr>
2016-02-11 11:22:34 +0100

    warehouse and product implementation

--- loic.c
+++ loic.c
@@ -3,18 +3,31 @@
 
 struct product {
     int type;
+    int amount;
     int weight;
 };
 
 struct warehouse {
     int x,y;
     struct product* products;
-    int weight;
 };
 
+struct warehouse* gen_warehouse(int x, int y, int prod_types, int* prod_present, int* weights) {
+    struct warehouse* warehouse = malloc(sizeof(struct warehouse));
+    struct product* products= malloc(sizeof(struct product) * prod_types);
+    for(size_t i = 0; i < prod_types; i++) {
+        products[i].type = i;
+        products[i].weight = weights[i];
+        products[i].amount = prod_present[i];
+    }
+    warehouse->x = x;
+    warehouse->y = y;
+    warehouse->products = products;
 
-int main(char[] argv, int* argc) {
+    return warehouse;
+}
+int main(int argc, char* argv[]) {
+    int** grid = malloc((int)argv[1]*(int)argv[2]*sizeof(int)); // TAKE X AND Y FROM INPUT
     
-
-
+    return 0;
 }
