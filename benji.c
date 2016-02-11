c6b674c437bab59f8664477f001bf17f53336b11
Benjamin Budet <budet_b@epita.fr>
2016-02-11 14:08:47 -0500

    files

--- benji.c
+++ benji.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 
 char* file_name = NULL;
-
-void readinfo(){
-    
-}
-
+int row = 0;
+int columns = 0;
+int drones = 0;
+int deadline = 0;
+int maxload = 0;
 int main(int argc, char* argv[]){
     if(argc < 2){
         errx(2,"Fail to open input");
     }
-    File *input = fopen(argv[2],"r+");
-    
+    FILE *input = fopen(argv[2],"r+");
+    char maxchar[10000];
+    int i = 0;
+    if (fgets (maxchar , 10000 , input) != NULL ){
+    }
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    while ((linelen = getline(&line, &linecap, input)) > 0){
+        maxchar[i] = linelen;
+        i++;
+    }
 }
