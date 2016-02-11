53a4ba57134fc5acb6aac6a26e3de5d2328bdf32
Benjamin Budet <budet_b@epita.fr>
2016-02-11 13:50:19 -0500

    add

--- benji.c
+++ benji.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-    File *input;
+char* file_name = NULL;
+
+void readinfo(){
+    
+}
+
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        errx(2,"Fail to open input");
+    }
+    File *input = fopen(argv[2],"r+");
+    
 }
