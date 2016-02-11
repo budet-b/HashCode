510611b8001dfd475a6c2749211b63429c407f9c
Loic Reyreaud <loic.reyreaud@epita.fr>
2016-02-11 10:49:37 +0100

    update loic.c

---
+++ loic.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct product {
+    int type;
+    int weight;
+};
+
+struct warehouse {
+    int x,y;
+    struct product* products;
+    int weight;
+};
+
+
+int main(char[] argv, int* argc) {
+    
+
+
+}
