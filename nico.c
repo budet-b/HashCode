efa5b47082843848415c3a86c2a7b792a0c0d261
Loic Reyreaud <loic.reyreaud@epita.fr>
2016-02-11 11:41:28 +0100

    updated function

--- nico.c
+++ nico.c
@@ -6,17 +6,18 @@ struct drone {
     int x,y;
     int maxWeight;
     int actualWeight;
+    struct product* products;
 
 }
 
-void cmd_Load(int id, int idWarehouse, int idProductType, int numberItems)
+void cmd_Load(struct drone* drone, struct warehouse* warehouse, int idProductType, int numberItems)
+{
+        
+}
+
+void cmd_Unload(struct drone* drone, struct warehouse* warehouse, int idProductType, int numberItems)
 {
     
-}
-
-void cmd_Unload(int id, int idWarehouse, int idProductType, int numberItems)
-{
-
 }
 void cmd_Deliver(int id, int idCustomer, int idProductType, int numberItems)
 {
